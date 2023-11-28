7d0799afc2d90a606caf01c66166f4025c79a763
Giles Weaver <git@wvr7.me.uk>
2023-11-28 16:50:51 +0000

    Update experiments/20230527_fuel_stacked_bar/fuel_type_stacked_bar_dask.py

--- experiments/20230527_fuel_stacked_bar/fuel_type_stacked_bar_dask.py
+++ experiments/20230527_fuel_stacked_bar/fuel_type_stacked_bar_dask.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.5
+#       jupytext_version: 1.15.2
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -17,12 +17,17 @@ import dask.dataframe as dd
 import pandas as pd
 from distributed import Client, progress, wait
 
-client = Client()
+client = Client(n_workers=6)
 client
 
 # +
 # Naive method ~3GB
-# 51s
+# 2023-06
+# 3.11.2 conda - 51s
+# 2023-11
+# 3.11.5 conda - 43.7s
+# 3.11.6 - 44.1s
+# 3.12.0 - 41s
 
 fuel_type_ddf = (
     dd.read_parquet(
@@ -41,9 +46,34 @@ fuel_type_ddf = (
 progress(fuel_type_ddf)
 
 # +
-# PyArrow optimised method ~1.7GB
-# 29s
+# PyArrow part optimised method ~1.7GB
+# 34s
 # Less RAM, less time
+# 24.6s
+
+fuel_type_ddf = (
+    dd.read_parquet(
+        "../../test_result.parquet",
+        dtype_backend="pyarrow",
+        columns=["test_result", "test_date", "fuel_type"],
+        # filters=[("test_result", "==", "P")],
+    )
+    .query('test_result == "P"')
+    .replace({"fuel_type": {"Hybrid Electric (Clean)": "HY", "Electric": "EL"}})
+    .assign(Year=lambda x: x.test_date.dt.year)
+    .groupby(["Year", "fuel_type"])
+    .agg({"test_result": "count"})
+    .rename(columns={"test_result": "vehicle_count"})
+    .persist()
+)
+
+progress(fuel_type_ddf)
+
+# +
+# PyArrow optimised method ~1.7GB
+# 29-32s
+# Less RAM, less time
+# 22.2s
 
 fuel_type_ddf = (
     dd.read_parquet(
